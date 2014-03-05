d7d54bac1ff8bc044c7b4332fe8b8848fd04cfec
Brian Grivna <sorghumking@yahoo.com>
2014-03-04 20:14:29 -0600

    Specify global dialect so only InnoDB tables are created/used; Pulled common properties into top-level dataSource block

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,89 +1,52 @@
 dataSource {
 	pooled = true
-	driverClassName = "org.hsqldb.jdbcDriver"
+	driverClassName = "com.mysql.jdbc.Driver"
 	username = "sa"
 	password = ""
+	dialect = "org.hibernate.dialect.MySQL5InnoDBDialect" // ensure InnoDB (not MyISAM!) table engine type
+	properties {
+		maxActive = 50
+		maxIdle = 25
+		minIdle = 5
+		initialSize = 5
+		minEvictableIdleTimeMillis = 900000
+		timeBetweenEvictionRunsMillis = 900000
+		maxWait = 10000
+		validationQuery = "select 1"
+		testOnBorrow = true
+		testWhileIdle = true
+	}
 }
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = true
     cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
-	//show_sql = true
-	//format_sql = true
-	//use_sql_comments = true
 }
-// environment specific settings
+
+// environment-specific settings
 environments {
     development {
-        //dataSource {
-        //    dbCreate = "create-drop" 
-        //    url = "jdbc:hsqldb:mem:devDB"
-        //}
 		dataSource {
 		    dbCreate = "update"
-			pooled = true
-		    driverClassName = "com.mysql.jdbc.Driver"
-			dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
 		    username = "tmi_web"
 		    password = "yctL3.cq]"
 			url = "jdbc:mysql://127.0.0.1:3306/tmi_test_myrboa?autoReconnect=true"
-			properties {
-				maxActive = 50
-				maxIdle = 25
-				minIdle = 5
-				initialSize = 5
-				minEvictableIdleTimeMillis = 900000
-				timeBetweenEvictionRunsMillis = 900000
-				maxWait = 10000
-				validationQuery = "select 1"
-				testOnBorrow = true
-				testWhileIdle = true
-			}
         }	
     }
     test {
         dataSource {
 		    dbCreate = "" //test db refreshed from prod
-			pooled = true
-		    driverClassName = "com.mysql.jdbc.Driver"
-			dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
 		    username = "tmi_web"
 		    password = "yctL3.cq]"
 			url = "jdbc:mysql://mysql.msi.umn.edu:3306/tmi_test_myrboa?autoReconnect=true"
-			properties {
-				maxActive = 50
-				maxIdle = 25
-				minIdle = 5
-				initialSize = 5
-				minEvictableIdleTimeMillis = 900000
-				timeBetweenEvictionRunsMillis = 900000
-				maxWait = 10000
-				validationQuery = "select 1"
-				testOnBorrow = true
-				testWhileIdle = true
-			}
         }	
     }
     production {
         dataSource {
 		    dbCreate = "update"
-			pooled = true
-		    driverClassName = "com.mysql.jdbc.Driver"
 		    username = "tmi_web"
 		    password = "yctL3.cq]"
 			url = "jdbc:mysql://mysql.msi.umn.edu:3306/tmi_prod_myrboa?autoReconnect=true"
-			properties {
-				maxActive = 50
-				maxIdle = 25
-				minIdle = 5
-				initialSize = 5
-				minEvictableIdleTimeMillis = 900000
-				timeBetweenEvictionRunsMillis = 900000
-				maxWait = 10000
-				validationQuery = "select 1"
-				testOnBorrow = true
-				testWhileIdle = true
-			}
 		}
     }
 }
